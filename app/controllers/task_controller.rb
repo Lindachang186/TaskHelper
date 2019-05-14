002c987ad2ecee540cab5c69aaa03e070809e8dd
github username <github email address>
2019-05-14 10:07:10 -0700

    created index and updated new for TaskController

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,7 +1,22 @@
 class TasksController < ApplicationController 
 
+    def index 
+      if params[:user_id] && !User.exists?(params[:user_id])
+        redirect_to users_path, alert: "User not found."
+      else
+        Task.all
+      end
+    end 
+
     def new 
-      @task = Task.new(user_id: params[:user_id])
+      if params[:user_id] && !User.exists?(params[:user_id])
+        redirect_to users_path, alert: "User not found."
+      else
+        @task = Task.new(user_id: params[:user_id])
+      end
+    end 
+
+    def edit 
     end 
 
     def create

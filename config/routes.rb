4fad1a66e1a1903292005635487c1bd6c2d74bfc
github username <github email address>
2019-05-17 13:59:19 -0700

    added bcrypt gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :tasks
   end 
 
+
   get '/', to: 'user#login'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_173335) do
+ActiveRecord::Schema.define(version: 2019_05_17_200628) do
 
   create_table "statuses", force: :cascade do |t|
     t.string "condition"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_05_10_173335) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
